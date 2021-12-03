880915c7dd3497032e8d8a3a06ff551bbc1f7697
Asit Barik <93936391+zacknight77@users.noreply.github.com>
2021-12-03 21:08:53 +0530

    Add files via upload

---
+++ inputandoutput-1.py
@@ -0,0 +1,5 @@
+#Input and Output-1
+list1 = [12, -7, 5, 64,-14]
+for i in list1:
+    if i >= 0:
+       print(i, end = " ")
---
+++ inputandoutput-2.py
@@ -0,0 +1,5 @@
+#Input and Output-2
+list2 = [12, 14, -95, 3]
+for i in list2:
+    if i >= 0:
+       print(i, end = " ")
